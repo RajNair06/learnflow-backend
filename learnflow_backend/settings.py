3471f4543ffdf6506e76996295fc9dc73d081a1c
Raj Nair <nraj00954@gmail.com>
2025-10-06 17:24:08 +0000

    configured celery for django

--- learnflow_backend/settings.py
+++ learnflow_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_celery_beat',
     
 ]
 
@@ -192,3 +193,10 @@ LOGGING={
     }
 
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
